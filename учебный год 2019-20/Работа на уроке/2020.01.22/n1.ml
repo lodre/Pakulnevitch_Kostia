724b3d8e3ce5ceb50698128e65c08c17f84e3e11
lodre <pakulnevitchkostia@yandex.ru>
2020-01-22 11:16:49 +0300

    kostia

--- учебный год 2019-20/Работа на уроке/2020.01.22/n1.ml
+++ учебный год 2019-20/Работа на уроке/2020.01.22/n1.ml
@@ -1,4 +1,4 @@
-let n = 10;;    
+let n = 4;;    
 let rec map f n = if n>0 then (f n) + (map f (n-1))
                          else 1;;
 let rec count_partition n = if n > 0 then map (fun x -> count_partition x) (n-1)
---
+++ учебный год 2019-20/Работа на уроке/2020.01.22/n3.ml
@@ -0,0 +1,9 @@
+let n = 4;;    
+let rec map f n x = if n>0 then (f (n,x+1)) + (map f (n-x) x)
+                           else if n = 0 then 1
+                                         else 0;;
+let rec count_partition n x = if x<=n then if n > 0 then map (fun (y,z) -> count_partition y z) n x
+                                                    else if n = 0 then 1
+                                                                  else 0
+                                      else 0;;
+print_int (count_partition n 1);;   
