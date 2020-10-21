55b271acec8ab172e0512fe3c3eb7654aaa89e9c
lodre <pakulnevitchkostia@yandex.ru>
2020-10-21 10:27:04 +0300

    kostia

---
+++ Учебный год 2020-21/ДЗ/ДЗ на 2020.10.12/n2.ml
@@ -0,0 +1,63 @@
+let p = 97;;
+let k = 11;;
+
+let hash x =
+let rec h s x i = if i<String.length x then h ((s*k+(int_of_char x.[i])) mod p) x (i+1)
+                                       else s in
+h 0 x 0;;  
+
+type ('a,'b) t = ('a*'b) list array ref;;
+
+let create hfunc size = ref (Array.make size []);;
+let add array key value = 
+	let h = hash key in
+	let (old_value, temp_list) = List.partition (fun (a,b) -> a = key) !array.(h) in
+	!array.(h) <- (key,value) :: temp_list;
+	match old_value with
+		[] -> None
+	      | [(k,v)] -> Some v
+	      | _ ->failwith ">= 2 equal values!";;
+                                                     
+let mem array key =
+	let h = hash key in
+	let (old_value, temp_list) = List.partition (fun (a,b) -> a = key) array.(h) in 
+	match old_value with
+		[] -> false
+	      | [(k,v)] -> true
+	      | _ ->failwith ">= 2 equal values!";;
+
+let find array key =
+	let h = hash key in
+	let (old_value, temp_list) = List.partition (fun (a,b) -> a = key) array.(h) in 
+	match old_value with
+		[] -> None
+	      | [(k,v)] -> Some v
+	      | _ ->failwith ">= 2 equal values!";;
+
+
+let delete array key = 
+	let h = hash key in
+	let (old_value, temp_list) = List.partition (fun (a,b) -> a = key) array.(h) in
+	array.(h) <- temp_list; 
+	match old_value with
+		[] -> None
+	      | [(k,v)] -> Some v
+	      | _ ->failwith ">= 2 equal values!";;
+
+
+let iter array fn = 
+	for i = 0 to (Array.length array) - 1 do
+		List.iter array.(i) fn
+	done;;
+                             
+        
+let rec fold array f x =
+	let rec lfold l f x = 
+	match l with
+	|(a,b)::c -> lfold c f (f (a,b) x) 
+	|[] -> x in
+		let y = ref x in
+		for i = 0 to (Array.length array) - 1 do
+			y := lfold (array.(i)) f !y
+		done; 
+	!y;;
