89f86e629856fdc1ac4e2285924c722978cf6ea8
lodre <pakulnevitchkostia@yandex.ru>
2020-12-14 11:20:48 +0300

    kostia

--- Учебный год 2020-21/ДЗ/ДЗ на 2020.09.14/n3.ml
+++ Учебный год 2020-21/ДЗ/ДЗ на 2020.09.14/n3.ml
@@ -1,5 +1,12 @@
 let f = open_in "n3.txt";;
                     
+let rec ends c = c = '.'||c = '!'||c = '?';;
+
+let rec rus s i r f = if i<String.length s then (if ends s.[i] then r>f
+                                                               else (if (((s.[i]>='à')&&(s.[i]<='ÿ'))||((s.[i]>='À')&&(s.[i]<='ß'))) then rus s (i+1) (r+1) f
+                                                                                                                         else (if (((s.[i]>='a')&&(s.[i]<='z'))||((s.[i]>='A')&&(s.[i]<='Z'))) then rus s (i+1) r (f+1) else rus s (i+1) r f)))
+                                           else r>f;;       
+
 let cap c = if ((c>='A' && c<='Z')||(c>='À' && c<='ß')) then 1
                                                         else 0;;
                                                                    
@@ -10,10 +17,11 @@ let rec next s i = if i < String.length s then (if (s.[i]<'A') then (if s.[i] = 
                                                                else next s (i+1))
                                           else (i,0);;                                                    
 
-let rec words_count s i (j,k) = if i < String.length s then (words_count s (fst (next s i)) (j+1,k+(snd (next s i))))
-                                                       else (j,k);;      
+let rec words_count s i j1 (j2,k2) (r,f) = if i < String.length s then (if snd (next s i) = 1 then (if r>=f then words_count s (fst (next s i)) 0 (j1+j2+1,k2+1) (0,0) else words_count s (fst (next s i)) 0 (j2,k2) (0,0))
+                                                                                              else words_count s (fst (next s i)) (j1+1) (j2,k2) (if rus s i 0 0 then (r+1,f) else (r,f+1)))
+                                                                  else (if r>=f then (j1+j2,k2) else (j2,k2));;      
 
-let g s = words_count s 0 (0,0);;
+let g s = words_count s 0 0 (0,0) (0,0);;
                                                        
 let rec read (x1,x2) = (try
                      let s = input_line f in let (y1,y2) = g s in
