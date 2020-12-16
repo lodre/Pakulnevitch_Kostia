e62b00c8c9599f43a091e2772a3c902608f4692c
lodre <pakulnevitchkostia@yandex.ru>
2020-12-16 19:51:21 +0300

    kostia

--- Учебный год 2020-21/Работа на уроке/Работа на уроке 2020.11.09/n3.ml
+++ Учебный год 2020-21/Работа на уроке/Работа на уроке 2020.11.09/n3.ml
@@ -1,15 +1,24 @@
 type tree = Tree of tree list;;
-let t = Tree [Tree []; Tree [Tree []]; Tree []];;
+let t = Tree [Tree []; Tree [Tree [Tree [];Tree []]]; Tree []; Tree[]; Tree[Tree[Tree[]]]];;
 
-let rec make_string s i = if i = 0 then ""
-                                   else s^(make_string s (i-1));;
-
-let rec print_tree t i =                                                
+let rec make_string l = match l with a::b -> (make_string b)^(if a = 1 then "| "
+                                                                       else "  ")
+                                    |[] -> "";;
+                                                                            (*
+let rec print_tree t i =                                                    
 match t with Tree []      -> ()
             |Tree (a::[]) -> (print_string ((if i = 0 then ""
                                                       else (make_string "| " i))^"\\-*\n"); print_tree a (i+1))
             |Tree (a::b)  -> ((print_string ((if i = 0 then ""
                                                        else (make_string "| " i))^"+-*\n"); print_tree a (i+1));print_tree (Tree b) i);;
+                                                                              *)
 
+let rec print_tree t l =                                                    
+match t with Tree []      -> ()
+            |Tree (a::[]) -> (print_string ((if l = [] then ""
+                                                      else (make_string l))^"\\-*\n"); print_tree a (0::l))
+            |Tree (a::b)  -> (print_string ((if l = [] then ""
+                                                       else (make_string l))^"+-*\n"); print_tree a (1::l);print_tree (Tree b) l);;
+                                                                              
 print_string "*\n";;
-print_tree t 0;;                                                                                                 
+print_tree t [];;                                                                                                 
